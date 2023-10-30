00057dff4534fb5fffa02ad81dfe4aad3a649f48
mohammadtarique6698 <mohammadtarique661998@gmail.com>
2023-10-30 18:43:39 +0530

    almost made responsive for all screens

--- src/components/LowerFooter/lowerfooter.jsx
+++ src/components/LowerFooter/lowerfooter.jsx
@@ -4,7 +4,7 @@ import "./lowerfooter.css"
 const lowerfooter = () => {
   return (
     <div className="lower-footer">
-        <h6>© 2020 Acme. All rig</h6>
+        <h6>© 2020 Acme. All right reserved</h6>
         <h6>Privacy Policy</h6>
         <h6>Terms of Service</h6>
     </div>
--- src/components/Subscribe/subscribe.jsx
+++ src/components/Subscribe/subscribe.jsx
@@ -7,7 +7,7 @@ const subscribe = () => {
         <div className="subscribe-container">
             <h1>Subscribe Our Newsletter</h1>
             <div className="input-div">
-            <input type="text" placeholder="Name" style={{padding: "0.5rem", width: "18rem", borderTopLeftRadius: "0.5rem", borderBottomLeftRadius: "0.5rem"}}></input>
+            <input type="text" placeholder="Name" style={{padding: "0.5rem", width: "auto", borderTopLeftRadius: "0.5rem", borderBottomLeftRadius: "0.5rem"}}></input>
             <input type="text" placeholder="Enter your Email" style={{padding: "0.5rem"}}></input>
             <button style={{backgroundColor: "var(--yellow-color)", borderTopRightRadius: "0.5rem", borderBottomRightRadius: "0.5rem", padding: "0.5rem", paddingLeft: "1rem", paddingRight: "1rem"}}>Send</button>
             </div>
